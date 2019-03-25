b153b20d3ec07a78e6971881f2bffd33cb96f7bf
Wilbur Suero <wilbur.himself@gmail.com>
2019-03-25 07:07:15 -0400

    refactor(coffee to js): change galleryselect to js

--- app/assets/javascripts/spina/admin/galleryselect.js
+++ app/assets/javascripts/spina/admin/galleryselect.js
@@ -1,20 +1,25 @@
-$(document).on 'click', '.gallery .item:not(.item-uploader)', ->
+$(document).on('click', '.gallery .item:not(.item-uploader)', function () {
+  var $form, checkbox, checked, count, gallery
   gallery = $(this).parents('.gallery')
-
-  if gallery.data('multiselect') != undefined
+  if (gallery.data('multiselect') !== void 0) {
     $(this).toggleClass('selected')
     checkbox = $(this).find('input:checkbox')
     checkbox.prop("checked", !checkbox.prop("checked"))
     $form = $(this).closest('form')
     count = $form.find('.item.selected').size()
-    if count > 0
-      $form.find('.gallery-select-counter').text("(#{count})")
-    else
-      $form.find('.gallery-select-counter').text("")
-  else
+    if (count > 0) {
+      return $form.find('.gallery-select-counter').text("(" + count + ")")
+    } else {
+      return $form.find('.gallery-select-counter').text("")
+    }
+  } else {
     checked = $(this).find('input').prop('checked')
     gallery.find('.item').removeClass('selected')
     gallery.find('.item input').prop('checked', false)
-    $(this).addClass('selected') unless checked
+    if (!checked) {
+      $(this).addClass('selected')
+    }
     $(this).find('input').prop('checked', !checked)
-    $(this).closest('form').submit()
+    return $(this).closest('form').submit()
+  }
+})
