6ddfbe50e5aa53c4412872a7b9c24142cb6f9faf
Wilbur Suero <wilbur.himself@gmail.com>
2019-03-25 07:05:39 -0400

    refactor(coffee to js): change forms.coffee to js

--- app/assets/javascripts/spina/admin/forms.js
+++ app/assets/javascripts/spina/admin/forms.js
@@ -1,7 +1,9 @@
-$(document).on 'click', '.structure-form-menu ul li a', (e) ->
+$(document).on('click', '.structure-form-menu ul li a', function (e) {
+  var $structureForm
   $structureForm = $(this).parents('.structure-form')
   $(this).parent('li').siblings().removeClass('active')
   $(this).parent('li').addClass('active')
   $structureForm.find('.structure-form-pane').removeClass('active')
   $($(this).attr('href')).addClass('active')
-  e.preventDefault()
+  return e.preventDefault()
+})
